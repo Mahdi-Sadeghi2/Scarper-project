6244f342a9ab21ab9743e8f7a7e4fa1938a13684
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-03-30 17:19:13 +0330

    Added some changes to file

--- basic_methods.py
+++ basic_methods.py
@@ -1,1 +1,10 @@
-BASE_URL = 'https://techcrunch.com/'
+BASE_URL = 'https://search.techcrunch.com/search?p={keyword}&b={output}'
+
+    
+DEFAULT_NUMBER_OF_PAGES = 5
+DEFAULT_KEYWORD_INPUT = 'games'
+
+NUMBER_OF_PAGES = int(input('Enter a number for pages:---> ') or DEFAULT_NUMBER_OF_PAGES)
+TOTAL_SEARCH_ITEMS = NUMBER_OF_PAGES * 10
+KEYWORD_INPUT = input('Enter your keyword for search:---> ') or DEFAULT_KEYWORD_INPUT
+
