89ae6c15bea9868bce2696fafbbcea62d45373a0
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-04-02 01:10:45 +0330

    Added some modifications

--- basic_methods.py
+++ basic_methods.py
@@ -1,6 +1,6 @@
 
 BASE_URL = 'https://search.techcrunch.com/search?p={keyword}&b={output}'
-BASE_CATEGORY_URL = 'https://techcrunch.com/category/{categories}/'
+BASE_CATEGORY_URL = 'https://techcrunch.com/wp-json/wp/v2/posts?page={pages}1&_embed=true&_envelope=true'
 
 
 DEFAULT_NUMBER_OF_PAGES = 5
