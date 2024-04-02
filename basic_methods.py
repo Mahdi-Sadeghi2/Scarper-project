03cfab62f49129be226c1bbd66d9556477bb990f
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-04-02 06:21:35 +0330

    Added some changes in file

--- basic_methods.py
+++ basic_methods.py
@@ -1,6 +1,5 @@
-
-BASE_URL = 'https://search.techcrunch.com/search?p={keyword}&b={output}'
-BASE_CATEGORY_URL = 'https://techcrunch.com/wp-json/wp/v2/posts?page={pages}1&_embed=true&_envelope=true'
+BASE_URL = 'https://search.techcrunch.com/search;_ylt=AwrFOlrOTAtmG90DT2ynBWVH;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3BhZ2luYXRpb24-?p={keyword}&pz=10&bct=0&b={output}&pz=10&bct=0&xargs=0'
+BASE_CATEGORY_URL = 'https://techcrunch.com/wp-json/wp/v2/posts?page={pages}&_embed=true&_envelope=true'
 
 
 DEFAULT_NUMBER_OF_PAGES = 5
