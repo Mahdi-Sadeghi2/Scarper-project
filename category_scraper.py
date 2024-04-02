aa315994118d8042fdcd8b43e984e60e8d44a518
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-04-02 06:21:49 +0330

    Added some modifications in code

--- category_scraper.py
+++ category_scraper.py
@@ -1,48 +1,74 @@
 from basic_methods import DEFAULT_KEYWORD_INPUT, DEFAULT_NUMBER_OF_PAGES
+import requests
+from bs4 import BeautifulSoup
 
 
-# def user_category_choose(category=None):
-#     CATEGORY_CHOOSE = int(input('Enter a number:\n1.startups\n2.venture\n3.security\n4.artificial-intelligence\n5.cryptocurrency\n6.apps\n7.space\n8.social\n9.biotech-health\n10.enterprise\n11.hardware\n12.transportation\n13.fintech\n14.robotics\n15.commerce\n16.climate\n17.transportation\n18.government-policy:---> '))
-#     category = CATEGORY_CHOOSE
-#     if CATEGORY_CHOOSE == 1:
-#         category = 'Startups'
-#     elif CATEGORY_CHOOSE == 2:
-#         category = 'Venture'
-#     elif CATEGORY_CHOOSE == 3:
-#         category = 'Security'
-#     elif CATEGORY_CHOOSE == 4:
-#         category = 'artificial-intelligence'
-#     elif CATEGORY_CHOOSE == 5:
-#         category = 'cryptocurrency'
-#     elif CATEGORY_CHOOSE == 6:
-#         category = 'Apps'
-#     elif CATEGORY_CHOOSE == 7:
-#         category = 'Space'
-#     elif CATEGORY_CHOOSE == 8:
-#         category = 'Social'
-#     elif CATEGORY_CHOOSE == 9:
-#         category = 'Biotech-health'
-#     elif CATEGORY_CHOOSE == 10:
-#         category = 'Enterprise'
-#     elif CATEGORY_CHOOSE == 11:
-#         category = 'Hardware'
-#     elif CATEGORY_CHOOSE == 12:
-#         category = 'Transportation'
-#     elif CATEGORY_CHOOSE == 13:
-#         category = 'Fintech'
-#     elif CATEGORY_CHOOSE == 14:
-#         category = 'Robotics'
-#     elif CATEGORY_CHOOSE == 15:
-#         category = 'Commerce'
-#     elif CATEGORY_CHOOSE == 16:
-#         category = 'Climate'
-#     elif CATEGORY_CHOOSE == 17:
-#         category = 'Transportation'
-#     elif CATEGORY_CHOOSE == 18:
-#         category = 'Government-policy'
-#     else:
-#         print('Wrong input')
-#     return category
+class SearchByCategory:
+    def __init__(self, base_url, pages,) -> None:
+        self.pages = pages
+        self.base_url = base_url
+
+    def search_loop(self):
+        total_results = 0
+        for i in range(1, self.pages+1):
+            response = requests.get(self.base_url.format(pages=i))
+            data = response.json()
+            for d in data['body']:
+                print('Category:', d['primary_category']['name'], '\n')
+                print('Title:', d['title']['rendered'], '\n')
+                cleaned_data = BeautifulSoup(
+                    d['excerpt']['rendered'], "html.parser").text
+                print('Discription:', cleaned_data)
+                date = d['date']
+                full_datetime = date
+                date, time = full_datetime.split("T")
+                print("Published Date:", date, '\n')
+                print("Published Time:", time, '\n')
+                author_name = d['yoast_head_json']
+                print("Author Name:", author_name['author'])
+                print('Image:',d['yoast_head_json']['og_image'][0]['url'])
+                print("Article link:",d['link'])
+                page_results = len(data['body'])
+                total_results += int(page_results / 10)
+                print('*' * 80)
+            print("Total results:", total_results)
+
+
+class SearchByUserInput:
+    def __init__(self, pages, url, keyword, total_search) -> None:
+        self.pages = pages
+        self.url = url
+        self.keyword = keyword
+        self.total_search = total_search
+
+    def search_engine(self):
+        for i in range(1,int(self.total_search * 10)+1):
+            response = requests.get(self.url.format(
+                keyword=self.keyword, output=i))
+            soup = BeautifulSoup(response.text, features="html.parser")
+            result = soup.findAll('h4', {'class': 'pb-10'})
+            date = soup.findAll('span', {'class': 'pl-15 bl-1-666'})
+            author = soup.findAll('span', style='color:#333; font-weight: 700')
+            body = soup.findAll('p', style="color: #777")
+            link = soup.findAll('a', {'class': 'fz-20 lh-22 fw-b'})
+            total_result_in_website = [span.text for div in soup.findAll(
+                'div', {'class': 'compPagination'}) for span in div.findAll('span')]
+
+            for re, de, auth, bo, l, t in zip(result, date, author, body, link, total_result_in_website):
+                print('article title:', re.text)
+                print('date posted:', de.text)
+                print('author:', auth.text)
+                print('summary:', bo.text)
+                print('link:', l['href'])
+                print('total results in website:', t)
+                print('total pages number:', self.pages)
+                print('total number of the result:', self.total_search)
+                total_results = ''.join(filter(str.isdigit, t))
+                print('*' * 80)
+            if self.total_search > int(total_results):
+                print('Your request should be less then the total website results:\n'
+                      'total results in website:', total_results, '\nTry again')
+                break
 
 
 page = list()
