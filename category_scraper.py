48647b3fb1504bba1b9fbb329a5483e86366c01c
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-04-08 15:45:49 +0330

    Modified some changes

--- category_scraper.py
+++ category_scraper.py
@@ -1,8 +1,7 @@
 from basic_methods import DEFAULT_KEYWORD_INPUT, DEFAULT_NUMBER_OF_PAGES
 import requests
 from bs4 import BeautifulSoup
-from datasaver.models import ArticlCategory, ArticleBasedSearch
-from django.utils.dateparse import parse_date, parse_time
+
 
 
 class SearchByCategory:
@@ -16,26 +15,26 @@ class SearchByCategory:
             response = requests.get(self.base_url.format(pages=i))
             data = response.json()
             for d in data['body']:
-                articel = ArticlCategory.objects.create(
 
-                    category=d['primary_category']['name'],
-                    title=d['title']['rendered'],
-                    discription=BeautifulSoup(
-                        d['excerpt']['rendered'], "html.parser").text,
-                    Published_Date=parse_date(d['date']),
-                    published_time=parse_time(d['date']),
-                    author=d['yoast_head_json']['author'],
-                    image_url=d['yoast_head_json']['og_image'][0]['url'],
-                    articl_link=d['link']
+                print('category=',d['primary_category']['name']),
+                print('title=',d['title']['rendered']),
+                print('discription=',BeautifulSoup(
+                    d['excerpt']['rendered'], "html.parser").text),
+                date_part,time_part = d['date'].split('T')
+                print('Published_Date=',(date_part)),
+                print('published_time=',(time_part)),
+                print('author=',d['yoast_head_json']['author']),
+                print('image_url=',d['yoast_head_json']['og_image'][0]['url']),
+                print('articl_link=',d['link']),
 
 
 
-                )
-                articel.save()
+                
                 page_results = len(data['body'])
                 total_results += int(page_results / 10)
                 print('*' * 80)
             print("Total results:", total_results)
+            
 
 
 class SearchByUserInput:
@@ -51,7 +50,7 @@ class SearchByUserInput:
                 keyword=self.keyword, output=i))
             soup = BeautifulSoup(response.text, features="html.parser")
             result = soup.findAll('h4', {'class': 'pb-10'})
-            date = soup.findAll('span', {'class': 'pl-15 bl-1-666'})
+            date = soup.findAll('span', style='color:#777')
             author = soup.findAll('span', style='color:#333; font-weight: 700')
             body = soup.findAll('p', style="color: #777")
             link = soup.findAll('a', {'class': 'fz-20 lh-22 fw-b'})
@@ -59,17 +58,16 @@ class SearchByUserInput:
                 'div', {'class': 'compPagination'}) for span in div.findAll('span')]
 
             for re, de, auth, bo, l, t in zip(result, date, author, body, link, total_result_in_website):
-                article = ArticleBasedSearch.objects.create(
-                    title=re.text,
-                    date_posted=de.text,
-                    authors=auth.text,
-                    summary=bo.text,
-                    link=l['href'],
-                    keywords=self.keyword,
-                    websit_total_results=t
-                )
-                article.save()
-                print('Saved articl', article)
+                print('title=',re.text),
+                print('date_posted=',de.text),
+                print('authors=',auth.text),
+                print('summary=',bo.text),
+                print('link=',l['href']),
+                print('keywords=',self.keyword),
+                print('websit_total_results=',t),
+                
+                
+                
                 # print('total results in website:', t)
                 print('total pages number:', self.pages)
                 print('total number of the result:', self.total_search)
@@ -79,6 +77,7 @@ class SearchByUserInput:
                 print('Your request should be less then the total website results:\n'
                       'total results in website:', total_results, '\nTry again')
                 break
+              
 
 
 page = list()
